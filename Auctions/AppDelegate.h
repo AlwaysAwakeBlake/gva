9eb717fe70080b9a18fc594ca4f59ea695ee068d
Blake Mabe <blakemabe@Blakes-MacBook-Pro.local>
2015-03-03 22:36:57 -0500

    Initial Commit

---
+++ Auctions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Auctions
+//
+//  Created by Blake Mabe on 3/3/15.
+//  Copyright (c) 2015 BlakeMabeApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Auctions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Auctions
+//
+//  Created by Blake Mabe on 3/3/15.
+//  Copyright (c) 2015 BlakeMabeApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
